41b8093ac4854de3efeb29456bf1330f09be75d4
lettda <lettda@vcu.edu>
2018-12-25 15:13:08 -0500

    Initial Commit
    First commit

---
+++ Assets/camFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour {
+
+    public Transform playerTransform;
+    public float speed;
+
+    public float minY;
+    public float minX;
+    public float maxY;
+    public float maxX;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = playerTransform.position; //set the position of the camera = to the current position of the player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerTransform != null) //if player gameObject doesn't exist (i.e. dead)
+        {
+
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX); //set max boundaries for the camera (whose position is = to the players)
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+
+            transform.position = Vector2.Lerp(transform.position, new Vector2(clampedX,clampedY), speed); //set the position of the camera = to the new position of the player gameObject
+        }
+
+	}
+}
---
+++ Assets/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float speed;
+	private Rigidbody2D rb;
+    private Vector2 moveAmount;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>(); //target the rigid body component on the game object
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //Create new vector 2 object to hold movement direction = to key direction pressed
+        moveAmount = moveInput.normalized * speed; //move in the moveInput direction at a given speed
+         
+	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime); //move the rigid body component from it's current position in the input direction at the divices framerate
+    }
+}
