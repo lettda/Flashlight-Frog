9ed8cbb1f2593516d10628c35971d8c862e86195
lettda <lettda@vcu.edu>
2018-12-27 12:32:52 -0500

    Bubbles and boundaries
    Set up framework for bubble movement and created game
    boundaries….bubble moves, but right to left and not up

---
+++ Assets/bubble.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bubble : MonoBehaviour {
+
+    public float speed;
+    public float timeToPop;
+    public int value;
+    private Vector2 destinationPosition;
+
+    public float minY;
+    public float maxY;
+    public float minX;
+    public float maxX;
+
+    [HideInInspector]
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        float randomX = Random.Range(minX, maxX);
+        destinationPosition = new Vector2(randomX, maxY);
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destinationPosition, speed * Time.deltaTime);
+        }
+    }
+}
